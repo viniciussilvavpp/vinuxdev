1d388b17aafa0102ce241524bd273f9e39848b34
viniciussilvavpp <viniciussilvavpp@gmail.com>
2026-06-28 14:06:30 -0300

    Aprendendo AstroJs

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import rehypeLazyLoadImage from './src/plugins/lazyLoadImage.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://viniciussilvavpp.github.io',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex, rehypeLazyLoadImage],
